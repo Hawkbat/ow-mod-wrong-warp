0be445bb528cec57f0c4b7709e642019c6b913c1
Insmoshable <MiJSchloeder@gmail.com>
2024-01-31 21:27:38 -0600

    Create VanillaStationaryProbeLauncher.cs
    Created public variables for StationaryProbeLaunchers

---
+++ WrongWarp/Components/VanillaStationaryProbeLauncher.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using WrongWarp.Configs;
+using WrongWarp.Utils;
+
+namespace WrongWarp.Components
+{
+    public class VanillaStationaryProbeLauncher : WrongWarpBehaviour
+    {
+        public string PropPath;
+        public float InitialDegreesY;
+        public bool LockInputY;
+        public bool PhotosOnly;
+        public bool ReturnToStartPos;
+
+        public Vector3 interactPointOffset;
+
+        private GameObject prop;
+        public override void WireUp()
+        {
+            if (!string.IsNullOrEmpty(PropPath))
+            {
+                prop = Mod.NewHorizonsApi.SpawnObject(transform.root.gameObject, GetComponentInParent<Sector>(), PropPath, Vector3.zero, Vector3.zero, 1f, false);
+                prop.transform.SetParent(transform, false);
+                prop.transform.localPosition = Vector3.zero;
+                prop.transform.localEulerAngles = Vector3.zero;
+                prop.transform.localScale = Vector3.one;
+                var launcher = prop.GetComponent<StationaryProbeLauncher>();
+                if (launcher != null) {
+                    launcher._verticalPivot.localEulerAngles = new Vector3(350, 0, 0);
+                    launcher._photosOnly = PhotosOnly;
+                    launcher._initialDegreesY = InitialDegreesY;
+                    launcher._lockInputY= LockInputY;
+                    launcher._returnToStartPos = ReturnToStartPos;
+                    launcher._interactVolume.transform.localPosition += interactPointOffset;
+
+                    launcher._initRotX = launcher.transform.localRotation;
+                    launcher._initRotY = launcher._verticalPivot.localRotation;
+                    launcher._localUpAxis = launcher.transform.parent.InverseTransformDirection(launcher.transform.up);
+                    launcher._degreesY = InitialDegreesY;
+                    launcher.transform.localRotation = Quaternion.AngleAxis(launcher._degreesX, launcher._localUpAxis) * launcher._initRotX;
+                    launcher._verticalPivot.localRotation = Quaternion.AngleAxis(launcher._degreesY, -Vector3.right) * launcher._initRotY;
+                }
+            }
+        }
+        void OnDrawGizmos()
+        {
+            Gizmos.color = Color.gray;
+            Gizmos.DrawSphere(transform.position, 0.25f);
+        }
+
+        void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawSphere(transform.position, 0.25f);
+        }
+    }
+}
