1d37ec2d070fae6e216f04077aba7a650eba399c
Joshua Thome <joshpthome@gmail.com>
2024-05-08 21:24:36 -0500

    Invert option for save flag sensors

--- WrongWarp/Components/SaveFlagSensor.cs
+++ WrongWarp/Components/SaveFlagSensor.cs
@@ -9,9 +9,14 @@ namespace WrongWarp.Components
     public class SaveFlagSensor : Sensor
     {
         public SaveDataFlag Flag;
+        public bool Invert;
 
         public override float ComputeStrength()
-            => Mod.SaveData[Flag] ? 1f : 0f;
+        {
+            var str = Mod.SaveData[Flag] ? 1f : 0f;
+            if (Invert) str = 1f - str;
+            return str;
+        }
 
         public override void WireUp()
         {
